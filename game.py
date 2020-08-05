de4c0fe08b1e3f9749158af68ae31219c83b85aa
unknown <11kalpesh.pawar@gmail.com>
2020-08-05 14:49:37 +0530

    floor done

--- game.py
+++ game.py
@@ -1,16 +1,35 @@
 import pygame, sys
 
+def draw_floor():
+    screen.blit(floor_surface,(floor_x_pos,900))
+    screen.blit(floor_surface,(floor_x_pos + 576,900))
+
 pygame.init()
-screen = pygame.display.set_mode((576,1024))
+screen = pygame.display.set_mode((576,968))
 
-
+bg_surface = pygame.image.load('assets/background-day.png').convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
+floor_surface = pygame.image.load('assets/base.png').convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
+floor_x_pos = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.quit:
             pygame.quit()
             sys.exit()
 
-            
+    screen.blit(bg_surface,(0,0))
+    floor_x_pos -= 0.5
+    screen.blit(floor_surface,(floor_x_pos,900))
+    draw_floor()
+    if floor_x_pos <= -576:
+        floor_x_pos = 0
+
+
+
+
+
+
     pygame.display.update()
     
 
