6fa9ffda05fe5d1b0835aeac979bd30e416ca346
unknown <11kalpesh.pawar@gmail.com>
2020-08-05 14:14:39 +0530

    Working

---
+++ game.py
@@ -0,0 +1,17 @@
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((576,1024))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            pygame.quit()
+            sys.exit()
+
+            
+    pygame.display.update()
+    
+
+ 
